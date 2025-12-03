deb97723f152726e1c4d78535f1632087aabbc1a
Barath Raj K <157716432+barathraj10@users.noreply.github.com>
2025-12-03 22:01:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,27 +7,33 @@ const makeChange = (c) => {
 		d:10,
 		q:25		
 	}
+	const result={
+		p:0,
+		n:0,
+		d:0,
+		q:0
+	}
 	while(c>0){
-		if(c/q>0){
-			obj.q=c/q;
-			c=c%q;
+		if(c/obj.q>0){
+			result.q=Math.floor(c/obj.q);
+			c=c%obj.q;
 		}
-		else if(c/d>0){
-			obj.d=c/d;
-			c=c%d;
+		else if(c/obj.d>0){
+			result.d=c/obj.d;
+			c=c%obj.d;
 		}
-		else if(c/n>0){
-			obj.n=c/n;
-			c=c%n;
+		else if(c/obj.n>0){
+			result.n=c/obj.n;
+			c=c%obj.n;
 		}
 		else{
-			obj.p=c/p;
-			c=c%p;
+			result.p=c/obj.p;
+			c=c%obj.p;
 		}
 		
 	}
 };
 
 // Do not the change the code below
-const c = prompt("Enter c: ");
+// const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
